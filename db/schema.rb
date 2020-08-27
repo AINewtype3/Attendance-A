29b2d014ccb1ccbd42f9ed2487f13573ff5d719f
AINewtype3 <mamesocute3@gmail.com>
2020-08-27 04:12:26 +0000

    Finish add-basic-info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200826034714) do
+ActiveRecord::Schema.define(version: 20200827035433) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20200826034714) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.datetime "basic_time", default: "2020-08-26 22:30:00"
+    t.datetime "work_time", default: "2020-08-26 23:00:00"
   end
 
 end
