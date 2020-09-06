62ac1d63dea36eaf10880a4314a6ee5718d7dd37
AINewtype3 <mamesocute3@gmail.com>
2020-09-06 14:56:21 +0000

    ヘッダーレイアウト調整,上長ユーザー作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@ User.create!(name: "管理者",
              password: "password",
              password_confirmation: "password",
              admin: true)
+             
+User.create!(name: "上長A",
+             email: "boss-a@email.com",
+             department: "管理部",
+             password: "password",
+             password_confirmation: "password")
+
+User.create!(name: "上長B",
+             email: "boss-b@email.com",
+             department: "管理部",
+             password: "password",
+             password_confirmation: "password")
 
 60.times do |n|
   name  = Faker::Name.name
