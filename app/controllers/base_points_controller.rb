bd7399823aaf60e7bdec27cf16750d68fd8f10fd
EC2 Default User <ec2-user@ip-172-31-23-95.ap-northeast-1.compute.internal>
2020-11-28 15:11:26 +0000

    拠点一覧修正機能実装

--- app/controllers/base_points_controller.rb
+++ app/controllers/base_points_controller.rb
@@ -8,5 +8,20 @@ class BasePointsController < ApplicationController
   end
   
   def update
+    @base_point = BasePoint.find(params[:id])
+    if @base_point.update_attributes(base_point_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to base_points_url
+    else
+      render :edit      
+    end
   end
+  
+  
+  private
+
+    def base_point_params
+      params.require(:base_point).permit(:base_point_number, :base_point_name, :attendance_type)
+    end
+  
 end
