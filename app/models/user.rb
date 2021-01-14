ebc8f816725f5bb8e5ebcd9d10067fc654fb34a2
EC2 Default User <ec2-user@ip-172-31-23-95.ap-northeast-1.compute.internal>
2021-01-14 00:00:35 +0000

    userモデルにCSVコード追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,4 +52,16 @@ class User < ApplicationRecord
       User.all
     end
   end
+  
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      user = find_by(id: row["id"]) || new
+      user.attributes = row.to_hash.slice(*updatable_attributes)
+      user.save!(validate: false)
+    end
+  end
+  
+  def self.updatable_attributes
+    ['name']
+  end
 end
